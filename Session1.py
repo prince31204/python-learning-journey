8fb8d55f6e9e696c437ae9284e0c11cefbb1799b
Prince Parmar <choudharyshatakshgujjar@gmail.com>
2026-03-18 19:25:49 +0530

    Session1 Data types in Python

--- Session1.py
+++ Session1.py
@@ -9,3 +9,15 @@ print('Hello',1,4,5,True,sep='/')
 #space reading manual like sep=''
 
 print('Hello',1,4,5,True,sep='/')
+
+#
+print('hello', end='_')
+print('world')
+
+
+# Data Types
+#Integer
+print(8)
+print(1e308)
+
+print(8.55)
