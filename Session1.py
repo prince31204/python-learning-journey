ac7070f1cf9e14a9eb3038880618000a9550d474
Prince Parmar <choudharyshatakshgujjar@gmail.com>
2026-03-18 19:20:38 +0530

    Session 1

---
+++ Session1.py
@@ -0,0 +1,11 @@
+#Python is a case sensitive langugage
+print("Hello Prince Parmar")
+print('salman khan')
+
+print(7.7)
+
+print('Hello',1,4,5,True,sep='/')
+
+#space reading manual like sep=''
+
+print('Hello',1,4,5,True,sep='/')
